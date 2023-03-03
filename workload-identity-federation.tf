8b139a1aae488412ddce1abbc0b0788096a66ad5
Marcelo Mendonça <mendonca2709@gmail.com>
2023-03-02 22:25:42 -0300

    Update references

--- workload-identity-federation.tf
+++ workload-identity-federation.tf
@@ -1,8 +1,8 @@
 locals {
-  add_github_workload_identity_federation = length(compact([var.github_workload_identity_federation.repository, var.github_workload_identity_federation.environment])) == 2
   github_repository                       = var.github_workload_identity_federation.repository
   github_environment                      = var.github_workload_identity_federation.environment
-  github_pool_id                          = local.add_github_workload_identity_federation ? substr("${split("/", var.github_workload_identity_federation.repository)[1]}-${var.name}", 0, 32) : ""
+  add_github_workload_identity_federation = length(compact([local.github_repository, local.github_environment])) == 2
+  github_pool_id                          = local.add_github_workload_identity_federation ? substr("${split("/", local.github_repository)[1]}-${var.name}", 0, 32) : ""
   # only allow workflows from this repository/environment combination to impersonate this service account
   github_attribute_condition = "assertion.repository == \"${local.github_repository}\" && assertion.environment == \"${local.github_environment}\""
   github_issuer_uri          = "https://token.actions.githubusercontent.com"
@@ -43,7 +43,7 @@ resource "google_iam_workload_identity_pool_provider" "provider" {
 resource "google_service_account_iam_member" "github_service_account_user" {
   count = local.add_github_workload_identity_federation ? 1 : 0
 
-  service_account_id = "projects/${var.project_id}/serviceAccounts/${local.resource_email_static}"
+  service_account_id = google_service_account.service_account.id
   # Allow all principals that match the provider's condition to impersonate this service account
   member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool[0].name}/*"
   role   = "roles/iam.workloadIdentityUser"
