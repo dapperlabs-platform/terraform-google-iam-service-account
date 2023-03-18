5d7bf887115873332bb782b9d3bd255d9b5a04dc
Marcelo Mendonça <mendonca2709@gmail.com>
2023-03-18 17:27:30 -0300

    Create env vars (#11)

--- workload-identity-federation.tf
+++ workload-identity-federation.tf
@@ -1,15 +1,16 @@
 locals {
   github_repository                       = var.github_workload_identity_federation.repository
   github_environment                      = var.github_workload_identity_federation.environment
-  add_github_workload_identity_federation = length(compact([local.github_repository, local.github_environment])) == 2
-  github_pool_id                          = local.add_github_workload_identity_federation ? substr("${split("/", local.github_repository)[1]}-${var.name}", 0, 32) : ""
+  add_github_workload_identity_federation = length(compact([local.github_repository, local.github_environment])) == 2 ? 1 : 0
+  github_pool_id                          = substr("${split("/", local.github_repository)[1]}-${var.name}", 0, 32)
   # only allow workflows from this repository/environment combination to impersonate this service account
   github_attribute_condition = "assertion.repository == \"${local.github_repository}\" && assertion.environment == \"${local.github_environment}\""
   github_issuer_uri          = "https://token.actions.githubusercontent.com"
+  formatted_account_id       = upper(replace(local.account_id, "-", "_"))
 }
 
 resource "google_iam_workload_identity_pool" "github_pool" {
-  count    = local.add_github_workload_identity_federation ? 1 : 0
+  count    = local.add_github_workload_identity_federation
   provider = google-beta
 
   project                   = var.project_id
@@ -20,7 +21,7 @@ resource "google_iam_workload_identity_pool" "github_pool" {
 }
 
 resource "google_iam_workload_identity_pool_provider" "provider" {
-  count    = local.add_github_workload_identity_federation ? 1 : 0
+  count    = local.add_github_workload_identity_federation
   provider = google-beta
 
   project                            = var.project_id
@@ -41,10 +42,26 @@ resource "google_iam_workload_identity_pool_provider" "provider" {
 }
 
 resource "google_service_account_iam_member" "github_service_account_user" {
-  count = local.add_github_workload_identity_federation ? 1 : 0
+  count = local.add_github_workload_identity_federation
 
-  service_account_id = google_service_account.service_account.id
+  service_account_id = local.service_account.name
   # Allow all principals that match the provider's condition to impersonate this service account
   member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool[0].name}/*"
   role   = "roles/iam.workloadIdentityUser"
+}
+
+resource "github_actions_environment_variable" "service_account_github_environment_variables" {
+  count         = local.add_github_workload_identity_federation
+  repository    = local.github_repository
+  environment   = local.github_environment
+  variable_name = "${local.formatted_account_id}_SERVICE_ACCOUNT"
+  value         = local.service_account.name
+}
+
+resource "github_actions_environment_variable" "workload_identity_provider_github_environment_variables" {
+  count         = local.add_github_workload_identity_federation
+  repository    = local.github_repository
+  environment   = local.github_environment
+  variable_name = "${local.formatted_account_id}_WORKLOAD_IDENTITY_PROVIDER"
+  value         = google_iam_workload_identity_pool_provider.provider[0].name
 }
